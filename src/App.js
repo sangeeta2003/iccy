5c224b3a1f96f5eff5139acd5ff5eb3a738a26d8
*pknowledge* <mishrasangeeta944@gmail.com>
2024-01-26 20:02:44 +0530

    add fonnt family

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-function App() {
+import React from 'react'
+
+
+const App = () => {
   return (
-    <div className="bg-black">
-     hello
-     {/* this is my icy project */}
-    </div>
-  );
+    <div>
+        this platgorm is used in the of this category and this is the main resources of this filed and this is called mai 
+        
+         house of the work in the follwing category
+        in this it is used inn the follwoing usd in the category 
+        <div>
+          it is used in the form of the long lastic form of the actual form in the 
+          department in the 
+          hey sir i am sangeeta from goverment college of engineering ,kahlahandi in the form of the actual form in the case of the 
+          hey sister and bother 
+          dear sister and brother in the form of the casual form invalid when there is nno correction in my mind so don't mind 
+          it is acctual in the form 
+          it is sosn=bcsz in the form of the mutual form in the form of the religion 
+          
+        </div>
+      </div>
+    
+  )
 }
 
-export default App;
+export default App
