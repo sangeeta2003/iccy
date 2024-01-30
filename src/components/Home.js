5f558838ed5c251be55a8314ce4bca3f603c98c0
*pknowledge* <mishrasangeeta944@gmail.com>
2024-01-30 18:00:58 +0530

    homepage.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='bg-red'>
+        <div className='bg-green'>
+          <div className='bg-black'>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,44 @@
 
 export default function Navbar() {
-    return (
-      <div className="bg-white-800">
-       <div>
-        {/* bgc */}
-    
-          <nav className="bg-white p-4 ">
-            <div className="container mx-auto flex justify-between items-center ">
-
-
-              <div className="space-x-7 flex items-center text-xl">
-                <a href="/" className="text-black">
-                  Home
-                </a>
-                <a href="/about" className="text-black">
-                  Menu
-                </a>
-                <a href="/skills" className="text-black">
-                 About Us 
-                </a>
-                <a href="/projects" className="text-black">
-                  Service
-                </a>
-              </div>
-              <div className="h-12 w-12  ml-auto">
-              <img src='/avatar.png' alt="logo" className="ml-auto"/>
-              </div>
-              
-            </div>
-          </nav>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div className="bg-white-800">
+     <div>
+      {/* bgc */}
+     
   
+        <nav className="bg-white mt-4 ">
+        
+          <div className="container mx-auto flex justify-between items-center ">
+          <div className="flex items-center"> 
+              <div className="h-12 w-12 mr-auto">
+                <img src='/logo.png' alt="logo" />
+              </div>
+              <h1 className="text-black font-bold ml-2 text-2xl">City</h1>
+            </div>
+
+            <div className="space-x-7 flex items-center text-xl ml-auto ">
+              <a href="/" className="text-black">
+                Home
+              </a>
+              <a href="/about" className="text-black ">
+                Menu
+              </a>
+              <a href="/skills" className="text-black">
+               About Us 
+              </a>
+              <a href="/projects" className="text-black">
+                Service
+              </a>
+            </div>
+            <div className="h-12 w-12  ml-auto">
+            <img src='/avatar.png' alt="avtar" className="ml-auto"/>
+            </div>
+            
+
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
