949b38f50179375ff1288349c487cdb9ac066a02
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-01 19:18:27 +0530

    home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,13 @@ const Home = () => {
       
       {/* Left side div */}
       <div className='h-3/4 w-1/2 p-10 mt-12 ml-11 pr-10'>
-      <p className='text-base text-orange-500 font-semibold mt-12'>Bike delivery</p>
-        <img src='delivery.png' alt='' className='w-1/4 h-1/4 object-contain rounded-md'/>
+        <div className='absolute flex mb-14 '>
+        <p className='text-base text-orange-500 font-semibold mt-12'>Bike delivery</p>
+        <img src='delivery.png' alt='delivery boy' className='w-1/5 h-1/5 object-contain rounded-md'/>
        
         
+        </div>
+     <div className='absolute mt-10'>
       <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor mt-10 pr-14">
           The Fastest <br></br> Delivery in
           <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
@@ -19,12 +22,12 @@ const Home = () => {
 
         <p className="text-base text-textColor text-center md:text-left md:w-[80%] mt-3 text-2xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
-          eaque <br></br>fugit distinctio est nam voluptatum architecto, porro iusto
-          deserunt <br></br>recusandae ipsa minus eos sunt, dolores illo repellat facere
+          eaque fugit distinctio <br></br>est nam voluptatum architecto, porro iusto
+          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
           suscipit!
         </p>
         </div>
-
+        </div>
       {/* Right side div with image */}
       <div className='h-3/4 w-1/4 relative ml-auto mr-10 mt-12'>
         <img src='/heroBg.png' alt='background' className='h-full w-full object-cover' />
