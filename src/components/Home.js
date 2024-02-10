f7e207184ebc30a78b24c90e871df1ba3ba882f7
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-10 11:07:06 +0530

    Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,6 +26,13 @@ const Home = () => {
           deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
           suscipit!
         </p>
+        <button
+          type="button"
+          className="bg-gradient-to-br mt-5
+          from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
         </div>
         </div>
       {/* Right side div with image */}
